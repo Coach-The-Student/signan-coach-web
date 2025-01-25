fc5a2c2b77f3884b592c052e6f4e3d4a1c73ffc0
Lensatom <ayomidetommiwa@gmail.com>
2025-01-25 03:56:35 +0100

    :tada: setup: project initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="w-full h-screen bg-gray-800 flex justify-center items-center">
+      <h2 className="text-6xl font-extrabold text-blue-400 animate-pulse">SIGNAN</h2>
+    </div>
+  )
+}
+
+export default App
